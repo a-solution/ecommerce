1542a239f052155d04f167b6d936ee8cc650a632
phantrunghieu.0194@gmail.com <phantrunghieu.0194@gmail.com@223f1bed-1684-d7ef-5d98-0e6473735358>
2015-03-07 07:43:04 +0000

    Newsletter Send Mail

--- trunk/catalog/controller/common/newsletter.php
+++ trunk/catalog/controller/common/newsletter.php
@@ -3,20 +3,44 @@
 class ControllerCommonNewsletter extends Controller {
 
     public function index() {        
+
         $json = array();        
         
-        if(isset($this->request->post))
-        {
-            $email = $this->request->post['email'];            
-            $json['success'] = $email . ' is added successfully';
-        }
-        else
-        {
-            $json['error'] = 'System Error';
-        }
+        $data = array();
         
+        $message = 'This email is sent automatically, please do not reply. \nThanks,\nASACA Team';
+        
+        $defaultSubject = 'Thanks you for visiting ASACA';
+        
+        $theRequest = $this->request->get;
+        //$theRequest = $this->request->post;
+        
+        $email = isset($theRequest['email']) ? $theRequest['email'] : ''; 
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            try {
+                $this->sendEmail($email, $defaultSubject, $message);
+                $json['success'] = $email . ' is added successfully';
+            }
+            catch(Exception $ex) {
+                $json['failed'] = 'Error Message: ' .$ex->getMessage();
+            }
+        }   
+        else{
+            $json['invalid'] = $email . ' is invalid email';
+        }
+        //$json['error'] = 'System Error';
         $this->response->addHeader('Content-Type: application/json');
         $this->response->setOutput(json_encode($json));
     }
 
+    private function sendEmail($toEmail, $subject='', $message=''){
+        $mail = new Mail($this->config->get('config_mail'));
+        $mail->setTo($toEmail);
+        $mail->setFrom($this->config->get('config_email'));
+        $mail->setSender($this->config->get('config_name'));
+        $mail->setSubject($subject);
+        $mail->setText(html_entity_decode($message, ENT_QUOTES, 'UTF-8'));
+        $mail->send();
+    }    
 }
