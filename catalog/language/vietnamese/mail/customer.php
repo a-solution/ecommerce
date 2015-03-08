f319e709b3d14a7bcbdc92cf14076cfa26570102
hung.nguyenduy2010@gmail.com <hung.nguyenduy2010@gmail.com@223f1bed-1684-d7ef-5d98-0e6473735358>
2015-03-08 10:49:52 +0000

    dich mail đăng ký

--- catalog/language/vietnamese/mail/customer.php
+++ catalog/language/vietnamese/mail/customer.php
@@ -1,16 +1,16 @@
 <?php
 // Text
-$_['text_subject']        = '%s - Thank you for registering';
-$_['text_welcome']        = 'Welcome and thank you for registering at %s!';
-$_['text_login']          = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_services']       = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
-$_['text_thanks']         = 'Thanks,';
-$_['text_new_customer']   = 'New customer';
-$_['text_signup']         = 'A new customer has signed up:';
+$_['text_subject']        = '%s - Thông báo tạo tài khoản thành công';
+$_['text_welcome']        = 'Chào mừng và cảm ơn bạn đã đăng ký tài khoản tại %s!';
+$_['text_login']          = 'Tài khoản của bạn đã được tạo thành công và bạn có thể đăng nhập bằng địa chỉ email và mật khẩu mà bạn đã đăng ký. Để đăng nhập bạn vui long nhấn vào link bên dưới:';
+$_['text_approval']       = 'Tài khoản của bạn phải được duyệt trước khi bạn có thể đăng nhập. Sau khi đã được duyệt bạn có thể đăng nhập bằng địa chỉ email và mật khẩu mà bạn đã đăng ký. Để đăng nhập bạn vui long nhấn vào link bên dưới:';
+$_['text_services']       = 'Sau khi đăng nhập, bạn có thể truy cập vào những chức năng như xem lịch sự đặt hàng, in chứng từ và sửa thông tin tài khoản của bạn.';
+$_['text_thanks']         = 'Cảm ơn,';
+$_['text_new_customer']   = 'Khách hàng mới';
+$_['text_signup']         = 'Một khách hàng mới vừa đăng ký:';
 $_['text_website']        = 'Web Site:';
-$_['text_customer_group'] = 'Customer Group:';
-$_['text_firstname']      = 'First Name:';
-$_['text_lastname']       = 'Last Name:';
+$_['text_customer_group'] = 'Nhóm khách hàng:';
+$_['text_firstname']      = 'Tên:';
+$_['text_lastname']       = 'Họ:';
 $_['text_email']          = 'E-Mail:';
-$_['text_telephone']      = 'Telephone:';
+$_['text_telephone']      = 'Điện thoại:';
