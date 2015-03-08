157ce72bdc480463421072056e3e76589ce064d1
hung.nguyenduy2010@gmail.com <hung.nguyenduy2010@gmail.com@223f1bed-1684-d7ef-5d98-0e6473735358>
2015-03-08 10:53:21 +0000

    Dich mail đổi mật khẩu

--- catalog/language/vietnamese/mail/forgotten.php
+++ catalog/language/vietnamese/mail/forgotten.php
@@ -1,5 +1,5 @@
 <?php
 // Text
-$_['text_subject']  = '%s - New Password';
-$_['text_greeting'] = 'A new password was requested from %s.';
-$_['text_password'] = 'Your new password is:';
+$_['text_subject']  = '%s - Cung cấp mật khẩu mới';
+$_['text_greeting'] = 'Một mật khẩu mới đã được cung cấp từ %s.';
+$_['text_password'] = 'Mật khẩu mới của bạn là:';
