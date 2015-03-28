dd1db4d7dd506090e0fc96e6ce1bbe2cbf868463
muagicodo <muagicodo@223f1bed-1684-d7ef-5d98-0e6473735358>
2015-03-28 05:01:48 +0000

    update layout and behavior

--- trunk/catalog/language/vietnamese/shipping/free.php
+++ trunk/catalog/language/vietnamese/shipping/free.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_title']       = 'Free Shipping';
-$_['text_description'] = 'Free Shipping';
+$_['text_title']       = 'Miễn phí vận chuyện';
+$_['text_description'] = 'Phí vận chuyển';
