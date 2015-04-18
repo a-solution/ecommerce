281fa8918d4e004782cb7988cefdcf112c3a9f42
hung.nguyenduy2010@gmail.com <hung.nguyenduy2010@gmail.com@223f1bed-1684-d7ef-5d98-0e6473735358>
2015-04-18 09:56:42 +0000

    add module recent

---
+++ admin/language/vietnamese/module/recent.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Recent';
+
+// Text
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified recent module!';
+$_['text_edit']        = 'Edit Recent Module';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify recent module!';
