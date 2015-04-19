3a988b945e3d4be9811cf1aa3b77827b5011c49a
muagicodo <muagicodo@223f1bed-1684-d7ef-5d98-0e6473735358>
2015-04-19 10:14:06 +0000

    update layout recent

--- trunk/catalog/controller/module/recent.php
+++ trunk/catalog/controller/module/recent.php
@@ -1,16 +1,21 @@
 <?php
+
 class ControllerModuleRecent extends Controller {
-	public function index() {		
-                if (isset($_COOKIE["asaca_product_viewed"])) {
-                    //parse json to array
-                    $viewed_products = json_decode($_COOKIE["asaca_product_viewed"], true);
-                    $data['recent'] = $viewed_products;
-                }else {
-                    $data['recent'] = "";
-                }                                
-                
-		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/recent.tpl')) {
-                    return $this->load->view($this->config->get('config_template') . '/template/module/recent.tpl', $data);		
-		}
-	}
+
+    public function index() {
+        if (isset($_COOKIE["asaca_product_viewed"])) {
+            //parse json to array
+            $viewed_products = json_decode($_COOKIE["asaca_product_viewed"], true);
+            $data['recent'] = $viewed_products;
+        } else {
+            $data['recent'] = "";
+        }
+        
+        $data['heading_title'] = "Sản phẩm đã xem";
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/recent.tpl')) {
+            return $this->load->view($this->config->get('config_template') . '/template/module/recent.tpl', $data);
+        }
+    }
+
 }
